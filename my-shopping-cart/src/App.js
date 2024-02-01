94f204e116c5b3cad594c33f7f86e92b56246a43
byunn90 <kayhanturk90@Live.com>
2024-02-01 22:14:05 +1100

    fruits finished

--- my-shopping-cart/src/App.js
+++ my-shopping-cart/src/App.js
@@ -6,16 +6,44 @@ const initialBasketAndFriend = [
     id: 118836,
     name: "Kayhan",
     image: "https://i.pravatar.cc/48?u=118836",
+    balance: 200,
   },
   {
     id: 933372,
     name: "Busra",
     image: "https://i.pravatar.cc/48?u=933372",
+    balance: 100,
   },
   {
     id: 399476,
-    name: "",
-    image: "",
+    name: "Noah",
+    image: "https://i.pravatar.cc/48?u=399476",
+    balance: 50,
+  },
+  {
+    id: 499476,
+    name: "Ava",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+  /// Fruits
+  {
+    id: 334411,
+    fruit: "apple",
+    price: 1,
+    image: "🍏",
+  },
+  {
+    id: 335512,
+    fruit: "Banana",
+    price: 2,
+    image: "🍌",
+  },
+  {
+    id: 336613,
+    fruit: "Tomato",
+    price: 1,
+    image: "🍅",
   },
 ];
 
