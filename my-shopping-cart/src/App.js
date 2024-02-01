cf237f8cc8ae5386c5db2045e69822bae984556b
byunn90 <kayhanturk90@Live.com>
2024-02-01 21:42:32 +1100

    setting up people and fruits with object array

--- my-shopping-cart/src/App.js
+++ my-shopping-cart/src/App.js
@@ -1,6 +1,23 @@
+import { useState } from "react";
+import "./index.css";
 // array Objects
-
-const initialBasketAndFriend = [{}];
+const initialBasketAndFriend = [
+  {
+    id: 118836,
+    name: "Kayhan",
+    image: "https://i.pravatar.cc/48?u=118836",
+  },
+  {
+    id: 933372,
+    name: "Busra",
+    image: "https://i.pravatar.cc/48?u=933372",
+  },
+  {
+    id: 399476,
+    name: "",
+    image: "",
+  },
+];
 
 export default function App() {
   return (
